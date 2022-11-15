b15372daa591459f34f0c32b93da1a78f169f4a6
wolfbeark <wolfbeark0327@gmail.com>
2022-11-15 17:01:45 +0900

    image test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,10 @@ import "./App.css";
 
 function App() {
   const test = process.env;
-  console.log(test);
-  console.log(window.location.origin);
-  console.log(process.env.PUBLIC_URL);
+
   return (
     <div className="App">
-      <img src={`/images/Default0.png`} alt="" />
+      <img src={`${process.env.PUBLIC_URL}/images/Default0.png`} alt="" />
     </div>
   );
 }
